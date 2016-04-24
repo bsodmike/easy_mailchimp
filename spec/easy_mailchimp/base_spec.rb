01b1a4c848c8b3ee5792e75d06f68e8f5490f08b
Michael de Silva <michael@mwdesilva.com>
2016-04-24 11:00:28 +0530

    WIP base spec

--- spec/easy_mailchimp/base_spec.rb
+++ spec/easy_mailchimp/base_spec.rb
@@ -3,13 +3,39 @@ require 'spec_helper'
 describe EasyMailchimp::Base do
   subject { described_class.new }
 
-  xdescribe '#create_member' do
-    let(:list_id) { "id" }
+  describe '#create_member' do
+    let(:config) do
+      double('config', {
+        logger: nil,
+        api_key: '123-us11',
+        api_endpoint: 'https://{{dc}}.fakeapi.com'
+      })
+    end
+    let(:list_id) { "id-123" }
+    let(:members) { double('members') }
 
-    it 'calls ' do
-      expect_any_instance_of(::Gibbon::Request).to receive(:lists).with(list_id)
+    it 'uses the Gibbon API to create a member, with the list ID passed' do
+      expect(EasyMailchimp).to receive(:config).and_return config
+      expect(EasyMailchimp::Logger).to receive(:new).and_return nil
+
+      expect_any_instance_of(::Gibbon::Request).to receive(:lists).with(list_id).and_return members
+      expect(members).to receive_message_chain(:members, :create)
 
       subject.create_member(list_id)
+    end
+
+    context 'if an exception occurs' do
+      let(:error) { 'Ran out of gas!' }
+
+      it 'logs and raises the exception' do
+        expect(EasyMailchimp).to receive(:config).and_return config
+        expect_any_instance_of(::Gibbon::Request).to \
+          receive(:lists).and_raise(::Gibbon::MailChimpError, error)
+
+        expect_any_instance_of(::EasyMailchimp::Logger).to receive(:info)
+
+        expect { subject.create_member(list_id) }.to raise_error(::Gibbon::MailChimpError)
+      end
     end
   end
 end
